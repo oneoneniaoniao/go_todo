b34b8b6e45238077d422c4a360ffe810879cbd36
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2024-09-01 08:55:10 +0900

    create repository

--- main.go
+++ main.go
@@ -10,35 +10,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/oneoneniaoniao/go_todo/src/domain/models"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/oneoneniaoniao/go_todo/src/infra/database/repositories"
+	"github.com/oneoneniaoniao/go_todo/src/infra/database"
 )
 
-type DBConfig struct {
-	User string
-	Password string
-	Host string
-	Port int
-	Table string
-}
-
-func getDBConfig() DBConfig {
-    port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
-    return DBConfig{
-        User: os.Getenv("DB_USER"),
-        Password: os.Getenv("DB_PASSWORD"),
-        Host: os.Getenv("DB_HOST"),
-        Port: port,
-		Table: os.Getenv("DB"),
-    }
-}
-
-func connectionDB() (*gorm.DB, error) {
-	config := getDBConfig();
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", config.User, config.Password, config.Host, config.Port, config.Table)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	return db, err
-}
 
 func errorDB(db *gorm.DB, c *gin.Context) bool {
 	if db.Error != nil {
